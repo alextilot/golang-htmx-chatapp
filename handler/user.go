9789275bcf79b7cfe1fe445f768f3352b3e12b22
alextilot <alextilot@gmail.com>
2024-07-14 18:50:47 -0500

    sync: swap computers

--- handler/user.go
+++ handler/user.go
@@ -1,50 +1,59 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 
+	"errors"
 	"github.com/alextilot/golang-htmx-chatapp/model"
 	"github.com/alextilot/golang-htmx-chatapp/services"
-	"github.com/alextilot/golang-htmx-chatapp/utils"
 	"github.com/alextilot/golang-htmx-chatapp/web"
 	"github.com/alextilot/golang-htmx-chatapp/web/forms"
-	"github.com/go-playground/validator/v10"
-
+	//	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+)
+
+var (
+	ErrorUsernameNotFound   = errors.New("Username not found.")
+	ErrorInvalidCredentials = errors.New("Incorrect username or password")
+	ErrorUserAuthentication = errors.New("Unable to authenticate user.")
 )
 
 func (h *Handler) Login(etx echo.Context) error {
 	req := &userLoginRequest{}
-	errMap := utils.NewUIError()
+	errs := forms.LoginErrorsModelView{
+		Username: nil,
+		Password: nil,
+		Other:    nil,
+	}
 
 	// Validate user inputs!
-	if err := req.bind(etx); err != nil {
-		errs := err.(validator.ValidationErrors)
-		for _, v := range errs {
-			errMap.Add(v.Field(), fmt.Sprintf("%v", v.Tag()))
-		}
-		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errMap))
-	}
+	// if err := req.bind(etx); err != nil {
+	// 	validation := err.(validator.ValidationErrors)
+	// 	for _, _ := range validation {
+	// 		//errs.Add(v.Field(), fmt.Sprintf("%v", v.Tag()))
+	// 	}
+	// 	return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errs))
+	// }
 
 	user, err := h.userService.GetByUsername(req.Username)
 	if err != nil {
-		errMap.Add("other", "Username not found")
-		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errMap))
+		errs.Other = ErrorUsernameNotFound
+		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errs))
 	}
+
 	if user == nil {
-		errMap.Add("other", "Invalid username")
-		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errMap))
+		errs.Other = ErrorInvalidCredentials
+		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errs))
 	}
 	if !user.CheckPassword(req.Password) {
-		errMap.Add("other", "Invalid password")
-		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errMap))
+		errs.Other = ErrorInvalidCredentials
+		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errs))
 	}
 
 	// JWT tokens for signed in users.
 	if err := services.GenerateTokensAndSetCookies(user, etx); err != nil {
-		errMap.Add("other", "Unable to authenticate")
-		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errMap))
+		errs.Other = ErrorUserAuthentication
+		return web.Render(etx, http.StatusUnauthorized, forms.LoginForm(errs))
 	}
 
 	etx.Response().Header().Set("HX-Redirect", "/chatroom")
@@ -54,27 +63,31 @@ func (h *Handler) Login(etx echo.Context) error {
 func (h *Handler) SignUp(etx echo.Context) error {
 	var user = &model.User{}
 	req := &userRegisterRequest{}
-	errMap := utils.NewUIError()
+	errs := forms.SignUpErrorsModelView{
+		Username: nil,
+		Password: nil,
+		Other:    nil,
+	}
 
 	// Validate user inputs!
-	if err := req.bind(etx, user); err != nil {
-		errs := err.(validator.ValidationErrors)
-		for _, v := range errs {
-			errMap.Add(v.Field(), fmt.Sprintf("%v", v.Tag()))
-		}
-		return web.Render(etx, http.StatusUnauthorized, forms.SignUpForm(errMap))
-	}
+	// if err := req.bind(etx, user); err != nil {
+	// 	validation := err.(validator.ValidationErrors)
+	// 	for _, v := range validation {
+	// 		errs.Add(v.Field(), fmt.Sprintf("%v", v.Tag()))
+	// 	}
+	// 	return web.Render(etx, http.StatusUnauthorized, forms.SignUpForm(errs))
+	// }
 
 	// Create user, this should error out if user already exists.
 	if err := h.userService.Create(user); err != nil {
-		errMap.Add("other", "Username already exists")
-		return web.Render(etx, http.StatusUnauthorized, forms.SignUpForm(errMap))
+		errs.Add("other", "Username already exists")
+		return web.Render(etx, http.StatusUnauthorized, forms.SignUpForm(errs))
 	}
 
 	// JWT tokens for signed in users.
 	if err := services.GenerateTokensAndSetCookies(user, etx); err != nil {
-		errMap.Add("other", "Unable to authenticate")
-		return web.Render(etx, http.StatusUnauthorized, forms.SignUpForm(errMap))
+		errs.Add("other", "Unable to authenticate")
+		return web.Render(etx, http.StatusUnauthorized, forms.SignUpForm(errs))
 	}
 
 	etx.Response().Header().Set("HX-Redirect", "/chatroom")
