aece5fc59769a8f3019864ad310de0f428894b35
alextilot <alextilot@gmail.com>
2024-07-17 20:22:19 -0500

    refactor: created chatroom handler

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/alextilot/golang-htmx-chatapp/services"
 	"github.com/alextilot/golang-htmx-chatapp/store"
 	"github.com/alextilot/golang-htmx-chatapp/web"
-	"github.com/alextilot/golang-htmx-chatapp/web/components"
 	"github.com/alextilot/golang-htmx-chatapp/web/views"
 
 	"github.com/labstack/echo/v4"
@@ -31,7 +30,7 @@ func main() {
 	userStore := store.NewUserStore(db)
 	messageStore := store.NewMessageStore(db)
 
-	h := handler.NewHandler(userStore)
+	h := handler.NewHandler(userStore, messageStore)
 
 	// Init web framework
 	e := router.New()
@@ -46,7 +45,6 @@ func main() {
 	e.Static("css", "web/css")
 	e.Static("images", "web/images")
 
-	// e.Use(services.EchoMiddlewareJWTKey())
 	e.GET("/", func(etx echo.Context) error {
 		user, _ := services.GetUserContext(etx)
 		return web.Render(etx, http.StatusOK, views.HomePage(user.IsLoggedIn))
@@ -63,7 +61,6 @@ func main() {
 	})
 
 	e.POST("/logout", func(etx echo.Context) error {
-		time.Sleep(1 * time.Second)
 		return h.Logout(etx)
 	})
 
@@ -82,18 +79,7 @@ func main() {
 	chatroom.Use(services.TokenRefresherMiddleware)
 	chatroom.GET("", func(etx echo.Context) error {
 		user, _ := services.GetUserContext(etx)
-		messages, err := messageStore.GetMostRecent(10)
-		if err != nil {
-			return etx.String(http.StatusBadGateway, "unable to pre populate chat messages")
-		}
-
-		var messageViews []*components.MessageComponentViewModel
-		for i := len(messages) - 1; i >= 0; i-- {
-			msg := messages[i]
-			tmp := components.NewMessageView(msg.Username, msg.Data, msg.Time, msg.Username == user.Username)
-			messageViews = append(messageViews, tmp)
-		}
-		return web.Render(etx, http.StatusOK, views.ChatroomPage(user.IsLoggedIn, messageViews))
+		return h.Chatroom(etx, user)
 	})
 
 	chatroom.GET("/ws", func(etx echo.Context) error {
