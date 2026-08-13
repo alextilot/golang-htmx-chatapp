bb40c8de3bf130370653f8dadea8c5efb91a2c84
alextilot <alextilot@gmail.com>
2026-08-12 21:26:37 -0500

    feat: ws router

---
+++ internal/router/ws.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"context"
+
+	"github.com/alextilot/golang-htmx-chatapp/internal/auth"
+	"github.com/alextilot/golang-htmx-chatapp/internal/handler/ws"
+
+	"github.com/labstack/echo/v5"
+)
+
+func registerWebSocketRoutes(
+	e *echo.Echo,
+	ctx context.Context,
+	h *ws.Handler,
+) {
+	authenticated := e.Group("/ws")
+	authenticated.Use(auth.RequireLogin)
+
+	authenticated.GET("/groups/:groupID", func(c *echo.Context) error {
+		return h.Group.ConnectWS(c, ctx)
+	})
+
+	authenticated.GET("/chat/:id", func(c *echo.Context) error {
+		return h.Chat.ConnectWS(c, ctx)
+	})
+}
